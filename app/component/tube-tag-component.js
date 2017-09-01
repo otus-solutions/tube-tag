e9faa8e1b76f6bd19978ab779c1973234930951b
Tiago Matana <tiago.matana@gmail.com>
2017-09-01 17:13:03 -0300

    Erro boolean

--- app/component/tube-tag-component.js
+++ app/component/tube-tag-component.js
@@ -1,15 +1,48 @@
 (function() {
-    'use strict';
-    angular
+  'use strict';
+  angular
     .module('tubeTagGenerator')
-    .component('tubeTag',{
-        templateUrl: 'app/component/tube-tag-template.html',
-        controller: Controller
+    .component('tubeTag', {
+      templateUrl: 'app/component/tube-tag-template.html',
+      controller: Controller
     });
+  Controller.$inject = [
+    '$scope'
+  ]
 
-    function Controller() {
-      var self = this;
+  function Controller($scope) {
+    var self = this;
+    self.$onInit = onInit;
+
+    self.isValid = isValid;
+    self.build = build;
+
+
+    function onInit() {
 
 
     }
+
+    function isValid() {
+      var inicio = $scope.inicio;
+      var fim = $scope.fim;
+      console.log(fim);
+
+      if (inicio < fim) {
+        return true;
+      } else {
+        return false;
+      }
+
+    }
+
+    function build() {
+      var status = self.isValid();
+      console.log(status);
+    }
+
+
+
+  }
+
 }());
